e94e251a587098b4353bdec93313498c58b08979
eladlevy42 <eladio2002@gmail.com>
2024-05-05 14:18:50 +0300

    To Do list

--- ToDoList/ToDoList/toDo.js
+++ ToDoList/ToDoList/toDo.js
@@ -3,29 +3,32 @@ let Done = JSON.parse(DoneJson);
 let listAsJson = localStorage.getItem("ToDoLst");
 let list = JSON.parse(listAsJson);
 list = JSON.parse(listAsJson);
-
 printList();
 
 function printList() {
   if (localStorage.getItem("ToDoLst") != "[]") {
     document.querySelector("#list").innerHTML = "Your To Do List: <br />";
     let counter = 0;
+    list.sort();
     for (let i of list) {
       document.querySelector(
         "#list"
-      ).innerHTML += `<li class="deleteItem" id = "item${counter}" onclick = "deleteItem(${counter})">${i}</li>>`;
+      ).innerHTML += `<li class="deleteItem" id = "item${counter}" onclick = "deleteItem(this)">${i}</li>`;
       counter++;
     }
   } else {
     document.querySelector("#list").innerHTML = "Your To Do List is Empty";
   }
 }
-function deleteItem(itemId) {
-  let deletedItem = document.querySelector(`#item${itemId}`).innerText;
+function deleteItem(item) {
+  list.sort();
+  let deletedItem = item.innerText;
+  let indexInLst = list.indexOf(deletedItem);
   Done.push(deletedItem);
+  Done.sort();
   DoneJson = JSON.stringify(Done);
   localStorage.setItem("Done", DoneJson);
-  list.splice(itemId, 1);
+  list.splice(indexInLst, 1);
   listAsJson = JSON.stringify(list);
   localStorage.setItem("ToDoLst", listAsJson);
   printList();
@@ -33,11 +36,15 @@ function deleteItem(itemId) {
 function reset() {
   list = [];
   listAsJson = JSON.stringify(list);
-  localStorage.setItem("ToDoLst", listAsJson);
+  Done = [];
+  DoneJson = JSON.stringify(Done);
+  localStorage.setItem("ToDoLst", "[]");
+  localStorage.setItem("Done", "[]");
   printList();
 }
 function addBtn() {
   let listItem = document.querySelector("#toDoItem").value;
+  document.querySelector("#toDoItem").value = "";
   if (list == null) {
     list = [];
   }
